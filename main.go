a4cf0807b8ba2aed5fab862c628608fcac4aa1bd
tagasouichiro <stitow14@gmail.com>
2017-04-12 22:44:08 +0900

    add grep and cut

--- main.go
+++ main.go
@@ -7,11 +7,22 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	file := flag.String("f", "files.csv", "read file")
+	grep := flag.String("g", "", "grep conditions")
+	cut := flag.String("c", "1", "output field colum num. delimiter :")
 	flag.Parse()
+
+	fields := strings.Split(*cut, ":")
+	nums, err := strToIntSlice(fields)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	f, err := os.Open(*file)
 	if err != nil {
 		log.Fatal(err)
@@ -27,6 +38,36 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Println(record)
+		if contains(record, *grep) {
+			for i, r := range record {
+				for _, num := range nums {
+					if i == num {
+						fmt.Print(r + ",")
+					}
+				}
+			}
+			fmt.Println()
+		}
 	}
+}
+
+func contains(iter []string, str string) bool {
+	for _, v := range iter {
+		if strings.Contains(v, str) {
+			return true
+		}
+	}
+	return false
+}
+
+func strToIntSlice(iter []string) ([]int, error) {
+	nums := make([]int, len(iter))
+	for i, v := range iter {
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+		nums[i] = num
+	}
+	return nums, nil
 }
